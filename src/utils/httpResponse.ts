ad418f53f984ae6bc4d1f433045b6bc0a2c93285
hope-ag <62467666+hope-ag@users.noreply.github.com>
2022-06-20 07:23:52 +0000

    updated translations

--- src/utils/httpResponse.ts
+++ src/utils/httpResponse.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
-import { isString } from 'lodash';
 
 export const sendSuccessResponse = (req: Request, res: Response, data: any, status = 200) => {
   const success = true;
-  res.status(status).json({ data: isString(data) ? req.t(data) : data, success });
+  res.status(status).json({ data: translateData(data, req), success });
 };
+
+function translateData(data: any, req: Request) {
+  if (data && typeof data === 'object' && data.message && typeof data.message === 'string') {
+    return { ...data, message: req.t(data.message) };
+  }
+  if (data && typeof data === 'string') {
+    return req.t(data);
+  }
+  return data;
+}
